9f6b136b4cce6ecf519ae9cbb10f72739c11c55a
Arbaz Khan <arbazkhangul123@gmail.com>
2021-07-29 14:57:05 -0700

    third

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
 
     if (email && password) {
-      const res = await fetch("/login", {
+      const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/login`, {
         method: "POST",
         headers: {
           "Content-type": "application/json",
