3767a02b624e856eb708b17216a13d381309ae62
Arbazkhangul <arbazkhangul123@gmail.com>
2021-08-05 14:15:33 +0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light ">
   <div className="container-fluid">
     <NavLink className="navbar-brand"  exact to="/">Navbar</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
